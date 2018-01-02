29e63793ab2d29e4bf395d7e4032752ae9ce8089
devika <rdevikabhandari@gmail.com>
2018-01-01 20:08:59 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  MatProgressBarModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './menu/menu.component';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatProgressBarModule, MatTableModule, MatPaginatorModule, MatSortModule,
+    MatToolbarModule,MatListModule,MatGridListModule,MatButtonModule,
+     BrowserAnimationsModule,MatCardModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
