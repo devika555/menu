f67787dc06aec281d5c75d4ec1addff82ab69b00
devika <rdevikabhandari@gmail.com>
2018-01-07 14:20:39 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,37 @@ import 'hammerjs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent } from './footer/footer.component';
+import {DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
 import {MatListModule} from '@angular/material/list';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     MatProgressBarModule, MatTableModule, MatPaginatorModule, MatSortModule,
     MatToolbarModule,MatListModule,MatGridListModule,MatButtonModule,
      BrowserAnimationsModule,MatCardModule,
-    FlexLayoutModule
+    FlexLayoutModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
